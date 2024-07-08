0c9cd1e04cd16f2d2e49433a708df8e10195851d
Shuji05210 <xbb5104tf@gmail.com>
2024-07-08 16:08:51 +0900

    0708授業終わり

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const field = document.querySelector('.container');
+const field = document.querySelectorAll('.container');
 
 console.log(field);
 
-const game = () => {
-    
-}
+// const game = () => {
+//     for (let i = 1; i < 9; i++) {
+//         if (i%2 === 0) {
+            
+//         }
+//         else { }
+//     }
+// }
 
 // field.addEventListener(click,game);
