b1c3be21b377367f37044185bf871fbffea9a127
Shuji05210 <xbb5104tf@gmail.com>
2024-07-08 16:11:20 +0900

    0708

--- script.js
+++ script.js
@@ -2,13 +2,3 @@ const field = document.querySelectorAll('.container');
 
 console.log(field);
 
-// const game = () => {
-//     for (let i = 1; i < 9; i++) {
-//         if (i%2 === 0) {
-            
-//         }
-//         else { }
-//     }
-// }
-
-// field.addEventListener(click,game);
